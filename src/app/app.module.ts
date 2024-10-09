d6e0836401988a4e429cfbfa2bd8a4016fcec06f
Federico Peralta <federico.peralta91@gmail.com>
2024-10-09 16:57:41 +0200

    S9-L3 EPIBLOG 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ActivePostComponent } from './components/active-post/active-post.component';
+import { InactivePostComponent } from './components/inactive-post/inactive-post.component';
+import { PostDetailComponent } from './components/post-detail/post-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    ActivePostComponent,
+    InactivePostComponent,
+    PostDetailComponent
   ],
   imports: [
     BrowserModule,
